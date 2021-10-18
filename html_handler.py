3d5d24d808df9bce2a3fc093cdf6419f747c17a1
Amal P Mathews <amalpmathews2003@gmail.com>
2021-10-18 13:41:31 +0530

    hidden-search-box added

--- html_handler.py
+++ html_handler.py
@@ -1,3 +1,7 @@
+import os
+import time
+import argparse
+
 template="""<div class="card">
 <img class="card-img-top" src="{img}" alt="Card image cap">
 <div class="card-body">
@@ -8,21 +12,56 @@ template="""<div class="card">
 <small class="text-muted">{lupd}</small>
 </div>
 </div>"""
-import os
-import time
-thedir=os.getcwd()
-dirs=[ name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name)) ]
-for dir in dirs:
-      if(not os.path.isfile(f'{dir}/index.html')):
-            continue
-      print(template.format(img="img.png",title=dir,desc="amal",lupd=time.ctime(os.path.getmtime(dir))))
-# lines=[]
-# with open("index.html","r") as f:
-#       lines= f.readlines()
 
-# for line in lines:
-#       print(line)
+basic_html_temp="""<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>{name}</title>
+      <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
 
-            
+</body>
+<script src="script.js"></script>  
+</html>
+"""
+desc_template="""{
+      Name:
+      Desc:
+}
+"""
+def func():
+      thedir=os.getcwd()
+      dirs=[ name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name)) ]
+      for dir in dirs:
+            if(not os.path.isfile(f'{dir}/index.html')):
+                  continue
+            print(template.format(img="img.png",title=dir,desc="amal",lupd=time.ctime(os.path.getmtime(dir))))
 
+
+
+def new_html_folder(dir):
+      parent_dir=os.getcwd()
+      os.mkdir(dir)
+      with open(f"{dir}/index.html","w") as f:
+            f.write(basic_html_temp.format(name=dir.capitalize()))
+      f=open(f"{dir}/style.css","w")
+      f.close()
+      f=open(f"{dir}/script.js","w")
+      f.close()
+      f=open(f"{dir}/desc.txt","w")
+      f.write(desc_template)
+      f.close()
+      print(f'{dir} created with index.html,style.css,script.js and desc.txt')
+
+
+parser = argparse.ArgumentParser()      
+
+parser.add_argument('--folder',help='create folder with given name')
+args=parser.parse_args()
+if(args.folder):
+      new_html_folder(args.folder)
